e51867cf5349201a17d48991d3fb9180ebe078e6
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-06-25 23:43:30 +0300

    try to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/matches-game/',
+    base: 'sveta-kryukova.github.io/matches-game/',
 })
